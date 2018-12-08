027413d09621921d6b4dd4ba56fc0730e690105a
Zoheir <zoheirkhonyagar@gmail.com>
2018-12-08 16:46:56 +0330

    fix a bug in main index

--- resources/views/main/main-page/components/last-news.blade.php
+++ resources/views/main/main-page/components/last-news.blade.php
@@ -8,58 +8,44 @@
                     <div class="section__header-element"><a class="link link-arrow" href="{{ route('news-index') }}"><span>اخبار ما را بخوانید</span></a></div>
                 </div>
                 <div class="range range-center range-30 range-flex">
-                    <div class="cell-xs-12 cell-sm-12 cell-md-6 cell-lg-3">
+                    <div class="cell-sm-12 cell-lg-4">
                         <article class="post-modern">
-                            <a class="post-modern__image-wrap" href="single-post.html"><img class="post-modern__image" src="images/post-2-365x215.jpg" alt="" width="764" height="215"></a>
+                            <a class="post-modern__image-wrap" href="single-post.html"><img class="post-modern__image" src="images/post-1-365x215.jpg" alt="" width="764" height="215"></a>
                             <div class="post-modern__main">
                                 <p class="post-modern__title"><a href="single-post.html">لورم ایپسوم متن ساختگی با تولید سادگی نامفهوم از صنعت چاپ و با</a></p>
                                 <p class="post-modern__text">لورم ایپسوم متن ساختگی با تولید سادگی نامفهوم از صنعت چاپ و با استفاده از طراحان گرافیک است. چاپگرها و متون بلکه روزنامه و مجله در ستون و سطرآنچنان که لازم است و برای شرایط فعلی تکنولوژی مورد نیاز و کاربردهای متنوع با هدف بهبود</p>
                                 <ul class="post-modern__meta">
-                                    <li><span class="icon icon-primary mdi mdi-clock"></span><a href="single-post.html">6 فروردین 1397</a></li>
-                                    <li><span class="icon icon-primary fl-justicons-visible6"></span><span>524</span></li>
-                                    <li> <span class="icon icon-primary mdi mdi-comment-outline"></span><a href="single-post.html">3</a></li>
+                                    <li><span class="icon icon-primary mdi mdi-clock"></span><a href="single-post.html">۶ فروردین ۱۳۹۷</a></li>
+                                    <li><span class="icon icon-primary fl-justicons-visible6"></span><span>۵۲۴</span></li>
+                                    <li> <span class="icon icon-primary mdi mdi-comment-outline"></span><a href="single-post.html">۳</a></li>
                                 </ul>
                             </div>
                         </article>
                     </div>
-                    <div class="cell-xs-12 cell-sm-12 cell-md-6 cell-lg-3">
+                    <div class="cell-sm-10 cell-lg-4">
                         <article class="post-modern">
-                            <a class="post-modern__image-wrap" href="single-post.html"><img class="post-modern__image" src="images/post-2-365x215.jpg" alt="" width="764" height="215"></a>
+                            <a class="post-modern__image-wrap" href="single-post.html"><img class="post-modern__image" src="images/post-1-365x215.jpg" alt="" width="764" height="215"></a>
                             <div class="post-modern__main">
                                 <p class="post-modern__title"><a href="single-post.html">لورم ایپسوم متن ساختگی با تولید سادگی نامفهوم از صنعت چاپ و با</a></p>
                                 <p class="post-modern__text">لورم ایپسوم متن ساختگی با تولید سادگی نامفهوم از صنعت چاپ و با استفاده از طراحان گرافیک است. چاپگرها و متون بلکه روزنامه و مجله در ستون و سطرآنچنان که لازم است و برای شرایط فعلی تکنولوژی مورد نیاز و کاربردهای متنوع با هدف بهبود</p>
                                 <ul class="post-modern__meta">
-                                    <li><span class="icon icon-primary mdi mdi-clock"></span><a href="single-post.html">6 فروردین 1397</a></li>
-                                    <li><span class="icon icon-primary fl-justicons-visible6"></span><span>524</span></li>
-                                    <li> <span class="icon icon-primary mdi mdi-comment-outline"></span><a href="single-post.html">3</a></li>
+                                    <li><span class="icon icon-primary mdi mdi-clock"></span><a href="single-post.html">۶ فروردین ۱۳۹۷</a></li>
+                                    <li><span class="icon icon-primary fl-justicons-visible6"></span><span>۵۲۴</span></li>
+                                    <li> <span class="icon icon-primary mdi mdi-comment-outline"></span><a href="single-post.html">۳</a></li>
                                 </ul>
                             </div>
                         </article>
                     </div>
-                    <div class="cell-xs-12 cell-sm-12 cell-md-6 cell-lg-3">
+                    <div class="cell-sm-10 cell-lg-4">
                         <article class="post-modern">
-                            <a class="post-modern__image-wrap" href="single-post.html"><img class="post-modern__image" src="images/post-2-365x215.jpg" alt="" width="365" height="215"></a>
+                            <a class="post-modern__image-wrap" href="single-post.html"><img class="post-modern__image" src="images/post-1-365x215.jpg" alt="" width="764" height="215"></a>
                             <div class="post-modern__main">
-                                <p class="post-modern__title"><a href="single-post.html">لورم ایپسوم متن ساختگی با تولید سادگی نامفهوم از صنعت</a></p>
+                                <p class="post-modern__title"><a href="single-post.html">لورم ایپسوم متن ساختگی با تولید سادگی نامفهوم از صنعت چاپ و با</a></p>
                                 <p class="post-modern__text">لورم ایپسوم متن ساختگی با تولید سادگی نامفهوم از صنعت چاپ و با استفاده از طراحان گرافیک است. چاپگرها و متون بلکه روزنامه و مجله در ستون و سطرآنچنان که لازم است و برای شرایط فعلی تکنولوژی مورد نیاز و کاربردهای متنوع با هدف بهبود</p>
                                 <ul class="post-modern__meta">
-                                    <li><span class="icon icon-primary mdi mdi-clock"></span><a href="single-post.html">6 فروردین 1397</a></li>
-                                    <li><span class="icon icon-primary fl-justicons-visible6"></span><span>524</span></li>
-                                    <li> <span class="icon icon-primary mdi mdi-comment-outline"></span><a href="single-post.html">3</a></li>
-                                </ul>
-                            </div>
-                        </article>
-                    </div>
-                    <div class="cell-xs-12 cell-sm-12 cell-md-6 cell-lg-3">
-                        <article class="post-modern">
-                            <a class="post-modern__image-wrap" href="single-post.html"><img class="post-modern__image" src="images/post-2-365x215.jpg" alt="" width="365" height="215"></a>
-                            <div class="post-modern__main">
-                                <p class="post-modern__title"><a href="single-post.html">لورم ایپسوم متن ساختگی با تولید سادگی نامفهوم از صنعت</a></p>
-                                <p class="post-modern__text">لورم ایپسوم متن ساختگی با تولید سادگی نامفهوم از صنعت چاپ و با استفاده از طراحان گرافیک است. چاپگرها و متون بلکه روزنامه و مجله در ستون و سطرآنچنان که لازم است و برای شرایط فعلی تکنولوژی مورد نیاز و کاربردهای متنوع با هدف بهبود</p>
-                                <ul class="post-modern__meta">
-                                    <li><span class="icon icon-primary mdi mdi-clock"></span><a href="single-post.html">6 فروردین 1397</a></li>
-                                    <li><span class="icon icon-primary fl-justicons-visible6"></span><span>524</span></li>
-                                    <li> <span class="icon icon-primary mdi mdi-comment-outline"></span><a href="single-post.html">3</a></li>
+                                    <li><span class="icon icon-primary mdi mdi-clock"></span><a href="single-post.html">۶ فروردین ۱۳۹۷</a></li>
+                                    <li><span class="icon icon-primary fl-justicons-visible6"></span><span>۵۲۴</span></li>
+                                    <li> <span class="icon icon-primary mdi mdi-comment-outline"></span><a href="single-post.html">۳</a></li>
                                 </ul>
                             </div>
                         </article>
