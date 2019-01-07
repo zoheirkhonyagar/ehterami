06ecad533121cfffa542d73ff716ea053537edac
Zoheir <zoheirkhonyagar@gmail.com>
2019-01-07 04:36:01 +0330

    fix style bug

--- resources/views/main/master/layouts/top-bar.blade.php
+++ resources/views/main/master/layouts/top-bar.blade.php
@@ -5,7 +5,7 @@
             <ul class="rd-navbar-items-list">
                 <li>
                     <div class="unit unit-spacing-xxs unit-horizontal">
-                        <div class="unit__left"><span class="icon icon-sm icon-primary fl-bigmug-line-two319"></span></div>
+                        <div class="unit__left"><span style="top:8px;" class="icon icon-sm icon-primary fl-bigmug-line-two319"></span></div>
                         <div class="unit__body">
                         @if (Auth::check())
                             <form style="padding :0;margin:0;" method="POST" action="{{ route('logout') }}" class="">
